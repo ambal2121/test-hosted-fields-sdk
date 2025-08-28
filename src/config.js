eb2a14f5288498493267efeeeab4456e75ced8e9
Vladyslav Tsebenko <vladyslav.pierce@gmail.com>
2025-08-28 09:12:24 +0300

    Initial commit

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const PAYADMIT_CONFIG = {
   // URL to load hosted fields from base SDK
-  HOSTED_FIELDS_URL: 'https://card-fields.paymentiq.io/1.0.51/index.html',
-  // HOSTED_FIELDS_URL: 'https://test-hostedpages.paymentiq.io/1.0.31/index.html',
+  // HOSTED_FIELDS_URL: 'https://card-fields.paymentiq.io/1.0.51',
+  HOSTED_FIELDS_URL: 'https://test-hostedpages.paymentiq.io/1.0.31/index.html',
 
   // Base backend PayAdmit API URL
   API_BASE_URL: 'http://localhost:8090/api/v1',
 
-  PAYADMIT_TOKEN: 'hfy5cKO1pt0Qsa6Cb31DqjJSCuX1p04g',
+  PAYADMIT_TOKEN: '',
 
   SERVICE: 'payadmit_card_service',
 
